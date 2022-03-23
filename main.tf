cb4700bfb9631af64da52f60488d9e6bb6d67bda
mlogallo <85250289+mlogallo@users.noreply.github.com>
2022-03-23 10:16:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "global" {
   backend = "remote"
   config = {
-    organization = "Netnology-MINT"
+    organization = "default"
     workspaces = {
       name = var.globalwsname
     }
